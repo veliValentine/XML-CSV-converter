d7cd23de63956361a07538a5d2bffe3b6aac2910
veliValentine <nikkevanukas1995@gmail.com>
2021-03-17 16:50:03 +0200

    refactor: own module for file reading and writing

---
+++ src/app/fileManipulation/FileReader.java
@@ -0,0 +1,49 @@
+package app.fileManipulation;
+
+import app.Logger;
+
+import java.io.File;
+
+public class FileReader {
+
+    private Logger logger;
+
+    public FileReader(Logger logger) {
+        this.logger = logger;
+    }
+
+    public File readXML() {
+        return readXML(PathHelper.RESOURCE_FOLDER + PathHelper.XML_FILE);
+    }
+
+    public File readXML(String path) {
+        try {
+            File file = readFile(path);
+            if (!isXML(file)) {
+                throw new Exception("File not XML");
+            }
+            return file;
+        } catch (Exception e) {
+            logger.errorToConsole(e.getMessage());
+        }
+        return null;
+    }
+
+    public File readFile(String path) {
+        try {
+            return new File(path);
+        } catch (Exception ex) {
+            logger.errorToConsole(ex.getMessage());
+        }
+        return null;
+    }
+
+    private boolean isXML(File file) {
+        return getFileExtension(file).equals("xml");
+    }
+
+    private String getFileExtension(File file) {
+        String fileName = file.getName();
+        return fileName.substring(fileName.lastIndexOf('.') + 1);
+    }
+}
---
+++ src/app/fileManipulation/PathHelper.java
@@ -0,0 +1,7 @@
+package app.fileManipulation;
+
+public class PathHelper {
+    static final String RESOURCE_FOLDER = "src/resource/";
+    static final String XML_FILE = "exampleInvoice.xml";
+    static final String CSV_FILE = "result.csv";
+}
