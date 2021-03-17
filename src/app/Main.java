0ac15f4c769fc46a54b4c19ce0902ff4ff1cc0da
veliValentine <nikkevanukas1995@gmail.com>
2021-03-17 16:58:19 +0200

    refactor: cleaned main to own modules

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,30 +1,17 @@
 package app;
 
+import app.documentReader.DocumentReader;
 import app.documentReader.RowReader;
+import app.fileManipulation.FileReader;
+import app.fileManipulation.FileWriter;
 import org.w3c.dom.Document;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.File;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Main {
-
-    private static String RESOURCE_FOLDER = "src/resource/";
-    private static String XML_FILE = "exampleInvoice.xml";
-    private static String STYLE_FILE = "styles.xsl";
-
     public static void main(String[] args) {
-        File file = readXML(RESOURCE_FOLDER + XML_FILE);
-        Document document = getDocument(file);
+        Logger logger = new Logger();
+        FileReader fileReader = new FileReader(logger);
+        Document document = DocumentReader.createDocument(fileReader.readXML(), logger);
         RowReader rowReader = new RowReader(document);
 
         StringBuilder sb = new StringBuilder();
@@ -32,74 +19,9 @@ public class Main {
         for (InvoiceRow row : rows) {
             sb.append(row.toCSVRow());
         }
-        System.out.println(sb.toString());
-        writeToCSV(sb.toString());
-    }
 
-
-    private static void writeToCSV(String input) {
-        try (PrintWriter writer = new PrintWriter((RESOURCE_FOLDER + "test.csv"))) {
-            writer.write(input);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    private static void writeToCSV(Document document) {
-        File style = readFile(RESOURCE_FOLDER + STYLE_FILE);
-        try {
-            StreamSource styleSource = new StreamSource(style);
-            Transformer transformer = TransformerFactory
-                    .newInstance()
-                    .newTransformer(styleSource);
-            Source source = new DOMSource(document);
-            Result outputTarget = new StreamResult(new File(RESOURCE_FOLDER + "x.csv"));
-            transformer.transform(source, outputTarget);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static Document getDocument(File file) {
-        try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            return builder.parse(file);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
-
-    private static File readXML(String path) {
-        try {
-            File file = readFile(path);
-            if (!isXML(file)) {
-                throw new Exception("NOT XML file");
-            }
-            return file;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
-
-    private static File readFile(String path) {
-        try {
-            return new File(path);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
-
-    private static boolean isXML(File file) {
-        return getFileExtension(file).equals("xml");
-    }
-
-    private static String getFileExtension(File file) {
-        String fileName = file.getName();
-        return fileName.substring(fileName.lastIndexOf('.') + 1);
+        FileWriter fileWriter = new FileWriter(logger);
+        fileWriter.writeToCSV(sb.toString());
     }
 }
 
